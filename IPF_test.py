ed44ab9d2268ecb56fd9c5521474df5bb2102dc1
Veozen <152989587+Veozen@users.noreply.github.com>
2025-05-18 18:14:36 -0400

    Update IPF_test.py

--- IPF_test.py
+++ IPF_test.py
@@ -1,10 +1,16 @@
 from generate_table import *
 from IPF import *
+import numpy as np
 
 
 # test IPF
 table = generate_random_table(4,8,scale=2)
 
-table, margins, constraints = aggregate_table(table, by=[0,1,2,3], var="value")    
+table, margins, constraints = aggregate_table(table, by=[0,1,2,3], var="value")   
 
-IPF(input=table, constraints=constraints, targets=margins, unit_id="unit_id", var="value", cons_id="cons_id", db_file=None, tol=1, maxIter=100)
+#rename margin column
+margins = margins.rename(columns={"value":"target"})
+margins["target"] +=  np.random.uniform(-2, 2, margins.shape[0])
+ 
+
+adjusted_table = IPF(input=table, constraints=constraints, targets=margins, unit_id="unit_id", var="value", cons_id="cons_id", db_file=None, tol=1, maxIter=100)
